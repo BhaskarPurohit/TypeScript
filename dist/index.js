18cd22324c9624255fbb2c4601cc374158b500e5
Bhaskar Purohit <bhaskarpurohit22@gmail.com>
2023-10-07 22:05:08 +0530

    new

--- dist/index.js
+++ dist/index.js
@@ -3,3 +3,11 @@ let a = 34;
 let check = false;
 let userName = "bhaskar";
 let newVar = "my name is Bhaskar";
+let age; //can be both string and number
+const func = (m, n) => {
+    return m * n; //returns number
+    return String(m * n); //returns string
+};
+const func2 = (m, n) => {
+    return m + n;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,14 @@
 let a:number  = 34
 let check:boolean = false
 let userName:string = "bhaskar"
-let newVar = <String>"my name is Bhaskar"
+let newVar = <string>"my name is Bhaskar"
+let age:string | number   //can be both string and number
+
+const func = (m:number,n:number) =>{
+    return m*n  //returns number
+    return String(m*n) //returns string
+}
+
+const func2 = (m:number, n:number):number =>{  //:number on the function means will return only number
+    return  m+n
+}
